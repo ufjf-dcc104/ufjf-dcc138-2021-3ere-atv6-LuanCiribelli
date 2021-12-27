413c6df553f3b71af913e284050a25407276b6dd
Luan <luanreisciribelli@gmail.com>
2021-12-27 15:28:32 -0300

    Adiciona Restrições a esquerda (ainda com erro), e acima e abaixo (funcionando)

--- js/Main.js
+++ js/Main.js
@@ -22,11 +22,11 @@ mapa1.carregaMapa(modeloMapa1);
 cena1.configuraMapa(mapa1);
 
 const pc = new Sprite({ x: 50, vx: 10 });
-//const en1 = new Sprite({ x: 140, color: "red" });
+const en1 = new Sprite({ x: 160,vx:-10 ,color: "red" });
 
 cena1.adicionar(pc);
-//cena1.adicionar(en1);
-//cena1.adicionar(new Sprite({ y: 80, color: "red" }));
+cena1.adicionar(en1);
+cena1.adicionar(new Sprite({ y: 70, color: "red" }));
 
 cena1.iniciar();
 
--- js/Sprite.js
+++ js/Sprite.js
@@ -39,7 +39,6 @@ export default class Sprite {
     this.y = this.y + this.vy * dt;
     this.mx = Math.floor(this.x / this.cena.mapa.SIZE);
     this.my = Math.floor(this.y / this.cena.mapa.SIZE);
-    
   }
 
   colidiuCom(outro) {
@@ -52,6 +51,11 @@ export default class Sprite {
   }
 
   aplicaRestricoes(dt) {
+    
+    this.aplicaRestricoesDireita(dt);
+    this.aplicaRestricoesEsquerda(dt);
+  }
+  aplicaRestricoesDireita(dt) {
     const SIZE = this.cena.mapa.SIZE;
     if (this.vx > 0) {
       const pmx = this.mx + 1;
@@ -63,11 +67,32 @@ export default class Sprite {
           W: SIZE,
           h: SIZE,
         };
-       this.cena.ctx.strokeStyle ='white';
-       this.cena.ctx.strokeRect(tile.x,tile.y,SIZE,SIZE)
+        this.cena.ctx.strokeStyle = "white";
+        this.cena.ctx.strokeRect(tile.x, tile.y, SIZE, SIZE);
         if (this.colidiuCom(tile)) {
           this.vx = 0;
           this.x = tile.x - tile.w / 2 - this.w / 2 - 1;
+        }
+      }
+    }
+  }
+  aplicaRestricoesEsquerda(dt) {
+    const SIZE = this.cena.mapa.SIZE;
+    if (this.vx < 0) {
+      const pmx = this.mx - 1;
+      const pmy = this.my;
+      if (this.cena.mapa.tiles[pmy][pmx] != 0) {
+        const tile = {
+          x: pmx * SIZE + SIZE / 2,
+          y: pmy * SIZE + SIZE / 2,
+          W: SIZE,
+          h: SIZE,
+        };
+        this.cena.ctx.strokeStyle = "white";
+        this.cena.ctx.strokeRect(tile.x, tile.y, SIZE, SIZE);
+        if (this.colidiuCom(tile)) {
+          this.vx = 0;
+          this.x = tile.x + tile.w / 2 + this.w / 2 + 1;
         }
       }
     }
