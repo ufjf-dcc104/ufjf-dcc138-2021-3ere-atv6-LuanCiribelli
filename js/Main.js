8d4d65cff72dc3a3abe0198770147ce2d1dc0317
Luan <luanreisciribelli@gmail.com>
2022-01-08 22:01:02 -0300

    Adiciona especializações de Cena

--- js/Main.js
+++ js/Main.js
@@ -6,6 +6,8 @@ import { mapa1 as modeloMapa1 } from "../maps/mapa1.js";
 import Mixer from "./Mixer.js";
 import InputManager from "./InputManager.js";
 import Game from "./Game.js";
+import CenaJogo from "./CenaJogo.js";
+import CenaCarregando from "./CenaCarregando.js";
 
 const input = new InputManager();
 const mixer = new Mixer(10);
@@ -30,8 +32,14 @@ input.configurarTeclado({
 
 const game = new Game(canvas,assets,input);
 
-const cena1 = new Cena(canvas, assets);
+const cena0 = new CenaCarregando(canvas, assets);
+const cena1 = new CenaJogo(canvas, assets);
+
+
+game.adicionarCena("carregando",cena0);
 game.adicionarCena("jogo",cena1);
+
+
 const mapa1 = new Mapa(10, 14, 32);
 
 mapa1.carregaMapa(modeloMapa1);
